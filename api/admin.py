e6e09de501b2482bb37163fad8e9363aedc1685b
João Paulo Carvalho <jjpaulo2@protonmail.com>
2021-01-11 19:33:12 -0300

    updating admin site

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from api.models import ( Sobre, 
                          ParagrafoDescricao, 
                          RedeSocial, 
@@ -8,7 +9,13 @@ from api.models import ( Sobre,
                          Habilidade,
                          Portfolio )
 
-# Register your models here.
+class MyAPIAdminSite(admin.AdminSite):
+    site_header = 'Administração do site'
+    site_title = 'Administração do site'
+    index_title = 'João Paulo Carvalho'
+
+admin.site = MyAPIAdminSite()
+
 admin.site.register(Sobre)
 admin.site.register(ParagrafoDescricao)
 admin.site.register(RedeSocial)
@@ -16,4 +23,13 @@ admin.site.register(Experiencia)
 admin.site.register(Educacao)
 admin.site.register(ProjetoEducacao)
 admin.site.register(Habilidade)
-admin.site.register(Portfolio)
+
+@admin.register(Portfolio, site=admin.site)
+class PortfolioAdmin(admin.ModelAdmin):
+    readonly_fields = ('previa_da_foto',)
+
+    def previa_da_foto(self, obj):
+        return mark_safe('<img src="{url}" style="max-width: 100%;"/>'.format(
+                url = obj.gif.url
+            )
+        )
--- api/apps.py
+++ api/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
+from django.contrib.admin.apps import AdminConfig
 
+class MyAPIAdminSiteConfig(AdminConfig):
+    default_site = 'website.admin.MyAPIAdminSiteConfig'
 
 class ApiConfig(AppConfig):
     name = 'api'
